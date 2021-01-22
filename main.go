b2a93d2a624882974f38972ed45330187ae3188b
tuyen tran <vat1906@gmail.com>
2021-01-22 07:24:50 +0700

    update ci

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-func Divine()  {
-	
-}
-
-func test1()  {
-	i:=0
-	i+=1
-	println("Test 1")
-
-	// coment
-	j:=0
-	j+=1
-	println(j)
-}
 func main() {
-	//referrence
-println("1233")
+
 }
